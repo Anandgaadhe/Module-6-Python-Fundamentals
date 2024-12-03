a015fc5ac33e12043bb8fe13b4f17796c5a3b89b
Anandgaadhe <anandgaadhe368@gmail.com>
2024-12-04 00:49:34 +0530

    Add files via upload

---
+++ python assingment/Core Python Concepts/1.py
@@ -0,0 +1,21 @@
+# Write a Python program to demonstrate the creation of variables and different data types.
+
+# Integer data type
+age = 27 
+print("Age:", age)  
+
+# Float data type
+height = 6.5 
+print("Height:", height)  
+
+# String data type
+name = "anand"  
+print("Name:", name)  
+
+# Boolean data type
+flag = True  
+print(" This Is boolean Value:", flag)  
+
+
+
+
---
+++ python assingment/Core Python Concepts/4.py
@@ -0,0 +1,9 @@
+# How to check the type of a variable dynamically using type().
+
+age = 23              
+height = 6.5        
+name = "anand" 
+
+print("The type of 'age' is:", type(age)) 
+print("The type of 'height' is:", type(height))  
+print("The type of 'name' is:", type(name))
