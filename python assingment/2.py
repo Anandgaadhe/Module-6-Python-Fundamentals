a9590c5b6656e784e212476caaae6c65bd08c277
Anandgaadhe <anandgaadhe368@gmail.com>
2024-12-09 23:09:48 +0530

    Delete python assingment/2.py

--- python assingment/2.py
+++
@@ -1,6 +0,0 @@
-"""10. Practical Example 2: Write a Python program to find the length of each string in List1. 
-11.  Practical Example 3: Write a Python program to find a specific string in the list using a simple 
-for loop and if condition. """
-
-lis = ['apple', 'banana','mango']
-print(len(lis))
